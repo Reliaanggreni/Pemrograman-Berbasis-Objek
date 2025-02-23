722b1409770280c582b14c86dfb638c4dc482fae
Reliaanggreni <153573365+Reliaanggreni@users.noreply.github.com>
2025-02-23 11:58:05 +0800

    Add files via upload

---
+++ Pertemuan 2/Class2.java
@@ -0,0 +1,41 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Other/File.java to edit this template
+ */
+
+/**
+ *
+ * @author AYU
+ */
+class Mahasiswa {
+    // Atribut (data) dengan akses private
+    private String nama;
+    private int nim;
+
+    // Konstruktor
+    public Mahasiswa(String nama, int nim) {
+        this.nama = nama;
+        this.nim = nim;
+    }
+   
+    
+    // Metode untuk menampilkan informasi
+    public void tampilkanInfo() {
+        System.out.println("Nama: " + nama + ", NIM: " + nim);
+    }
+}
+
+public class Class2 {
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String args[]) {
+        // Membuat objek Mahasiswa
+        Mahasiswa mhs1 = new Mahasiswa("Budi", 220001);
+        Mahasiswa mhs2 = new Mahasiswa("Siti", 220002);
+
+        // Menampilkan informasi mahasiswa
+        mhs1.tampilkanInfo();
+        mhs2.tampilkanInfo();
+    }
+}
---
+++ Pertemuan 2/tugas1.java
@@ -0,0 +1,72 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Other/File.java to edit this template
+ */
+
+/**
+ *
+ * @author AYU
+ */
+
+    class Kendaraan {
+    // Atribut private untuk menyimpan informasi kendaraan
+    private String merk;
+    private String tipe;
+    private int kecepatan;
+
+    // Konstruktor untuk menginisialisasi objek Kendaraan
+    public Kendaraan (String merk, String tipe, int kecepatan) {
+    this.merk = merk;
+    this.tipe = tipe;
+    this.kecepatan = kecepatan;
+}
+    // Metode untuk menampilkan informasi kendaraan
+    public void tampilkanInfo() {
+       System.out.println("merk: " + merk + ", Tipe: " + tipe + ", kecepatan: " + kecepatan + " km/jam");
+    }
+    // Metode untuk menambah kecepatan kendaraan
+    public void tambahKecepatan(int km) {
+        this.kecepatan += km;
+        System.out.println(merk + " menambah kecepatan menjadi " + kecepatan + "km/jam");
+}
+    // Metode untuk mengurangi kecepatan kendaraan
+    public void kurangiKecepatan(int km) {
+        this.kecepatan -= km;
+        // Pastikan kecepatan tidak negatif
+        if (kecepatan < 0) {
+            kecepatan = 0;
+        }
+        System.out.println(merk + " mengurangi kecepatan menjadi " + kecepatan + "km/jam");
+}
+    // Metode untuk menghentikan kendaraan
+    public void berhenti() {
+        this.kecepatan = 0;
+        System.out.println("kendaraan telah berhenti.");
+}
+    }
+    public class tugas1{
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String args[]) {
+        Kendaraan mobil = new Kendaraan("Toyota", "mobil", 60);
+        Kendaraan motor = new Kendaraan("Yamaha", "motor", 40);
+        
+        // Menampilkan informasi mobil
+        mobil.tampilkanInfo();
+        mobil.tambahKecepatan(20);
+        mobil.kurangiKecepatan(30);
+        mobil.berhenti();
+        mobil.tampilkanInfo();
+        
+        System.out.println();
+        
+       // Menampilkan informasi motor
+        motor.tampilkanInfo();
+        motor.tambahKecepatan(30);
+        motor.kurangiKecepatan(40);
+        motor.berhenti();
+        motor.tampilkanInfo();
+    }
+    }
+     
