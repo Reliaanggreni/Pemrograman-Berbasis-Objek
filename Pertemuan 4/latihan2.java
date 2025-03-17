ec87caabaef7848059fe5aff6692f18cbccf241b
Reliaanggreni <153573365+Reliaanggreni@users.noreply.github.com>
2025-03-17 10:20:13 +0800

    Update latihan2.java

--- Pertemuan 4/latihan2.java
+++ Pertemuan 4/latihan2.java
@@ -12,12 +12,12 @@
         
        void makan(){
        System.out.println("Hewan sedang makan. ");
-    }
+        }
     }
     class Kucing extends Hewan{
         void suara(){
         System.out.println("Meow!");
-    }
+        }
     }
     class KucingAnggora extends Kucing{
         void jenis()
